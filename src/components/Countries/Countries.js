766bae712a50cea1e9eb5cfdaf9eb6d2adc169ce
mnaofficial <mna.official92@gmail.com>
2022-03-17 11:45:52 +0600

    final, Done

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -8,36 +8,36 @@ const Countries = () => {
             .then(res => res.json())
             .then(data => setCountries(data))
     }, [])
-    console.log(countries[0])
+    // console.log(countries[0])
     return (
-        <div>
-            <h1 className='header-text'>Visiting every country of the world!</h1>
-            <h4>Available country: {countries.length}</h4>
+        <div className='body-container'>
+            <div className='header-area'>
+                <h1 className='header-text'>Visiting every country of the world!</h1>
+                <p>Web Degine and Development: <span className='developer'>M N A</span></p>
+                <h4>Available country: {countries.length}</h4>
+            </div>
             <div className='main'>
                 {
-                    countries.map(country =>
-                        <Country
-                            name={country.name.common}
-                            official={country.name.official}
-                            flags={country.flags.png}
-                            population={country.population}
-                            capital={country.capital}
-                            continents={country.continents}>
-                        </Country>)
+                    countries.map(country => <Country
+                        country={country}
+                        key={country.cca3}>
+                    </Country>)
                 }
             </div>
         </div>
     );
 };
-function Country(props) {
+const Country = (props) => {
+    const { name, flags, capital, continents, area, population } = props.country;
     return (
         <div className='country'>
-            <img className='flags' src={props.flags} alt="" />
-            <h2>{props.name}</h2>
-            <p>Official Name: {props.official}</p>
-            <p>Capital: {props.capital}</p>
-            <p>Population: {props.population}</p>
-            <p>Continents: {props.continents}</p>
+            <h2>{name.common}</h2>
+            <img className='flags' src={flags.png} alt="" />
+            <p>Official Name: {name.official}</p>
+            <p>Capital: {capital}</p>
+            <p>Area: {area} km²</p>
+            <p>Population: {population}</p>
+            <p>Continents: {continents}</p>
         </div>
     )
 }
